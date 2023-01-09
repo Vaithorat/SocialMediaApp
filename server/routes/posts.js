c485d5c10413f74c0f5c4f7cf1fcfa71bec950c3
Vaithorat <vaibhavthorat141997@gmail.com>
2023-01-09 21:59:32 +0530

    added fake data

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts";
-import { verifyToken } from "../middleware/auth";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
-const router = exppress.Router();
+const router = express.Router();
 
 // Read
 router.get("/", verifyToken, getFeedPosts);
