b8d5b11b27b6e3610c55995f9dbc7f66c371c488
Vaithorat <vaibhavthorat141997@gmail.com>
2023-01-12 16:09:52 +0530

    typo

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -6,14 +6,13 @@ import {
   LocationOnOutlined,
 } from "@mui/icons-material";
 
-import { Box, Typography, Divider, userTheme } from "@mui/material";
+import { Box, Typography, Divider, useTheme } from "@mui/material";
 import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useTheme } from "@emotion/react";
 
 const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
